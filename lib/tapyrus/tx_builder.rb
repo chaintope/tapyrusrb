0d5b236dc0c0a7bbf12dc199c3c296fd821d1932
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2021-04-21 00:17:27 +0900

    Add class/method documentation for TxBuilder

--- lib/tapyrus/tx_builder.rb
+++ lib/tapyrus/tx_builder.rb
@@ -1,4 +1,35 @@
 module Tapyrus
+  #
+  # Transaction Builder class.
+  #
+  # TxBuilder makes it easy to  build transactions without having to deal with TxOut/TxIn/Script directly.
+  #
+  # @example
+  #
+  #   txb = Tapyrus::TxBuilder.new
+  #   utxo1 = {
+  #     script_pubkey: Tapyrus::Script.parse_from_addr('mgCuyNQ1pUbKqL57tJQZX3hhUCaZcuX3RQ'),
+  #     txid: 'e1fb3255ead43dccd3ae0ac2c4f81b32260ca52749936a739669918bbb895411',
+  #     index: 0,
+  #     value: 3_000
+  #   }
+  #   color_id = Tapyrus::Color::ColorIdentifier.nft(...)
+  #   utxo2 = {
+  #     script_pubkey: Tapyrus::Script.parse_from_addr('mu9QMUcB9UCHbQjZJLAuyysQhM9tmFQbPx'),
+  #     color_id: color_id,
+  #     txid: 'e1fb3255ead43dccd3ae0ac2c4f81b32260ca52749936a739669918bbb895411',
+  #     index: 1,
+  #     value: 3_000
+  #   }
+  #
+  #   tx = txb
+  #     .add_utxo(utxo1)
+  #     .add_utxo(utxo2)
+  #     .data("0102030405060a0b0c")
+  #     .reissuable(utxo1[:script_pubkey],'n4jKJN5UMLsAejL1M5CTzQ8npeWoLBLCAH', 10_000)
+  #     .pay('n4jKJN5UMLsAejL1M5CTzQ8npeWoLBLCAH', 1_000)
+  #     .build
+  #
   class TxBuilder
     def initialize
       @utxos = []
@@ -8,7 +39,7 @@ module Tapyrus
     end
 
     # Add utxo for transaction input
-    # @param utxo
+    # @param utxo [Hash] a hash whose fields are `txid`, `index`, `script_pubkey`, `value`, and `color_id` (color_id is optional)
     def add_utxo(utxo)
       @utxos << utxo
       color_id = utxo[:color_id] || Tapyrus::Color::ColorIdentifier::default
@@ -18,21 +49,27 @@ module Tapyrus
     end
 
     # Issue reissuable token
+    # @param script_pubkey [Tapyrus::Script] the script pubkey in the issue input.
     # @param address [String] p2pkh or p2sh address.
+    # @param value [Integer] issued amount.
     def reissuable(script_pubkey, address, value)
       color_id = Tapyrus::Color::ColorIdentifier.reissuable(script_pubkey)
       pay(address, value, color_id)
     end
 
     # Issue non reissuable token
+    # @param out_point [Tapyrus::OutPoint] the out point at issue input.
     # @param address [String] p2pkh or p2sh address.
+    # @param value [Integer] issued amount.
     def non_reissuable(out_point, address, value)
       color_id = Tapyrus::Color::ColorIdentifier.non_reissuable(out_point)
       pay(address, value, color_id)
     end
 
     # Issue NFT
+    # @param out_point [Tapyrus::OutPoint] the out point at issue input.
     # @param address [String] p2pkh or p2sh address.
+    # @param value [Integer] issued amount.
     def nft(out_point, address)
       color_id = Tapyrus::Color::ColorIdentifier.nft(out_point)
       pay(address, 1, color_id)
@@ -40,8 +77,8 @@ module Tapyrus
 
     # Create payment output.
     # @param address [String] tapyrus address with Base58 format
-    # @param value [Integer]
-    # @param color_id [Tapyrus::Color::ColorIdentifier]
+    # @param value [Integer] issued or transferred amount
+    # @param color_id [Tapyrus::Color::ColorIdentifier] color id
     def pay(address, value, color_id = Tapyrus::Color::ColorIdentifier::default)
       script_pubkey = Tapyrus::Script.parse_from_addr(address)
 
@@ -74,10 +111,10 @@ module Tapyrus
       self
     end
 
-    # Set script_pubkey for change.
-    # If set, #build method add output for change which has the specified script_pubkey
+    # Set address for change.
+    # If set, #build method add output for change which has the specified address
     # If not set, transaction built by #build method has no output for change.
-    # @param script_pubkey [Tapyrus::Script] p2pkh or p2sh
+    # @param address [String] p2pkh or p2sh address.
     def change_script_pubkey(script_pubkey)
       raise ArgumentError, 'invalid address' if !script_pubkey.p2pkh? && !script_pubkey.p2sh?
       @change_script_pubkey = script_pubkey
