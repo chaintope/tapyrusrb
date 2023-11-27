db2504df92c347691a3850571663f1f708793bad
Yamaguchi <h_yamaguchi@chaintope.com>
2023-11-27 14:24:24 +0900

    Fix for review comment
    * Allow color_id to be nil
    * Add method comment for #sign_message and #verify_message
    * Change method name sign_message => sign_messageverify_message => verify_message!
    * Change error type to ArgumentError

--- lib/tapyrus/tip0137.rb
+++ lib/tapyrus/tip0137.rb
@@ -9,13 +9,23 @@ module Tapyrus
     # @param address [String] Tapyrus address for transaction output
     # @param data [String] hexdecimal string
     # @param client [Tapyrus::RPC::TapyrusCoreClient] rpc client
-    def sign_message(
+    # @return [String] Signed message with JWS Format
+    # @raise [ArgumentError] raise if txid is not a 64-character hexadecimal string
+    # @raise [ArgumentError] raise if index is not a non-negative integer less than 2^32
+    # @raise [ArgumentError] raise if color_id is an invalid Tapyrus::Color::ColorIdentifier object
+    # @raise [ArgumentError] raise if value is not a non-negative integer less than 2^64
+    # @raise [ArgumentError] raise if script_pubkey is an invalid Tapyrus::Script object
+    # @raise [ArgumentError] raise if address is an invalid Tapyrus address
+    # @raise [ArgumentError] raise if data is not a hexdecimal string
+    # @raise [ArgumentError] raise if transaction is not found with txid and index in blockchain 
+    # @raise [ArgumentError] raise if script and value is not equal to ones in blockchain
+    def sign_message!(
       key,
-      txid: nil,
-      index: nil,
+      txid:,
+      index:,
+      value: ,
+      script_pubkey:,
       color_id: nil,
-      value: nil,
-      script_pubkey: nil,
       address: nil,
       data: nil,
       client: nil
@@ -51,26 +61,35 @@ module Tapyrus
       JWS.encode(data, key.priv_key)
     end
 
-    #   # @param jws [String] JWT Web Token
-    #   # @param key [Tapyrus::Key] public key
-    #   # @param client [Tapyrus::RPC::TapyrusCoreClient] rpc client
-    #   # @return decoded JSON Object
-    def verify_message(jws, key, client: nil)
+    # @param jws [String] JWT Web Token
+    # @param key [Tapyrus::Key] public key
+    # @param client [Tapyrus::RPC::TapyrusCoreClient] rpc client
+    # @return decoded JSON Object
+    # @raise [ArgumentError] raise if decoded txid is not a 64-character hexadecimal string
+    # @raise [ArgumentError] raise if decoded index is not a non-negative integer less than 2^32
+    # @raise [ArgumentError] raise if decoded color_id is an invalid Tapyrus::Color::ColorIdentifier object
+    # @raise [ArgumentError] raise if decoded value is not a non-negative integer less than 2^64
+    # @raise [ArgumentError] raise if decoded script_pubkey is an invalid Tapyrus::Script object
+    # @raise [ArgumentError] raise if decoded address is an invalid Tapyrus address
+    # @raise [ArgumentError] raise if decoded data is not a hexdecimal string
+    # @raise [ArgumentError] raise if transaction is not found with decoded txid and index in blockchain 
+    # @raise [ArgumentError] raise if decoded script and value is not equal to ones in blockchain
+    def verify_message!(jws, key, client: nil)
       JWS
         .decode(jws, key.pubkey)
         .tap do |decoded|
           payload = decoded[0]
           color_id =
             begin
-              Tapyrus::Color::ColorIdentifier.parse_from_payload(payload['color_id']&.htb)
+              Tapyrus::Color::ColorIdentifier.parse_from_payload(payload['color_id']&.htb) if payload['color_id']
             rescue => _e
-              raise RuntimeError, 'color_id is invalid'
+              raise ArgumentError, 'color_id is invalid'
             end
           script_pubkey =
             begin
               Tapyrus::Script.parse_from_payload(payload['script_pubkey']&.htb)
             rescue => _e
-              raise RuntimeError, 'script_pubkey is invalid'
+              raise ArgumentError, 'script_pubkey is invalid'
             end
           validate_payload!(
             txid: payload['txid'],
@@ -94,37 +113,37 @@ module Tapyrus
         end
     end
 
-    def validate_payload!(txid: nil, index: nil, color_id: nil, value: nil, script_pubkey: nil, address: nil, data: nil)
-      raise RuntimeError, 'txid is invalid' if !txid || !/^[0-9a-fA-F]{64}$/.match(txid)
-      raise RuntimeError, 'index is invalid' if !index || !/^\d+$/.match(index.to_s) || index < 0 || index >= 2**32
-      if !color_id || !color_id.is_a?(Tapyrus::Color::ColorIdentifier) || !color_id.valid?
-        raise RuntimeError, 'color_id is invalid'
+    def validate_payload!(txid:, index:, value:, script_pubkey:, color_id: nil, address: nil, data: nil)
+      raise ArgumentError, 'txid is invalid' if !txid || !/^[0-9a-fA-F]{64}$/.match(txid)
+      raise ArgumentError, 'index is invalid' if !index || !/^\d+$/.match(index.to_s) || index < 0 || index >= 2**32
+      if color_id && (!color_id.is_a?(Tapyrus::Color::ColorIdentifier) || !color_id.valid?)
+        raise ArgumentError, 'color_id is invalid'
       end
-      raise RuntimeError, 'value is invalid' if !value || !/^\d+$/.match(value.to_s) || value < 0 || value >= 2**64
-      if !script_pubkey || !script_pubkey.is_a?(Tapyrus::Script) || !/^([0-9a-fA-F]{2})+$/.match(script_pubkey.to_hex)
-        raise RuntimeError, 'script_pubkey is invalid'
+      raise ArgumentError, 'value is invalid' if !value || !/^\d+$/.match(value.to_s) || value < 0 || value >= 2**64
+      if !script_pubkey || !script_pubkey.is_a?(Tapyrus::Script)
+        raise ArgumentError, 'script_pubkey is invalid'
       end
       begin
         address && Base58.decode(address)
       rescue ArgumentError => e
-        raise RuntimeError, 'address is invalid'
+        raise ArgumentError, 'address is invalid'
       end
-      raise RuntimeError, 'data is invalid' if data && !/^([0-9a-fA-F]{2})+$/.match(data)
+      raise ArgumentError, 'data is invalid' if data && !/^([0-9a-fA-F]{2})+$/.match(data)
     end
 
     def validate_on_blockchain!(client: nil, txid: nil, index: nil, color_id: nil, value: nil, script_pubkey: nil)
       raw_tx = client.getrawtransaction(txid)
       tx = Tapyrus::Tx.parse_from_payload(raw_tx.htb)
       output = tx.outputs[index]
-      raise RuntimeError, 'output not found in blockchain' unless output
+      raise ArgumentError, 'output not found in blockchain' unless output
       if color_id != output.color_id
-        raise RuntimeError, 'color_id of transaction in blockchain is not match to one in the signed message'
+        raise ArgumentError, 'color_id of transaction in blockchain is not match to one in the signed message'
       end
       if value != output.value
-        raise RuntimeError, 'value of transaction in blockchain is not match to one in the signed message'
+        raise ArgumentError, 'value of transaction in blockchain is not match to one in the signed message'
       end
       if script_pubkey != output.script_pubkey
-        raise RuntimeError, 'script_pubkey of transaction in blockchain is not match to one in the signed message'
+        raise ArgumentError, 'script_pubkey of transaction in blockchain is not match to one in the signed message'
       end
     end
   end
